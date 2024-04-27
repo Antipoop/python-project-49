9e7ec979aec0a035b909f316da55b95f9daf5b80
Antipoop <antipoop2000@yandex.ru>
2024-04-27 21:52:17 +0300

    (step5) adds: + game brain_even + function welcome_user in cli.py now return username

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,2 +1,24 @@
+from brain_games.cli import welcome_user
+import random
+
 def brain_even():
-    print('lox')
+    name = welcome_user()
+    print('Answer "yes" if the number is even, otherwise answer "no".')
+    i = 0
+    while i < 3:
+        num = random.randint(1, 100)
+        if num % 2 == 0:
+            correct_answer = 'yes'
+        else:
+            correct_answer = 'no'
+        print(f'Question:{num}')
+        user_answer = input('Your answer:')
+        if correct_answer != user_answer:
+            print(f"'{user_answer}' is wrong answer ;(. Correct answer was '{correct_answer}'.\nLet's try again, {name}!")
+            return
+        print('Correct!')
+        i += 1
+    print(f'Congratulations, {name}!')
+
+def main():
+    return brain_even()
